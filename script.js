e9d90df7abda3b7e50b3cbea86c43f8238f09b06
jv40 <139980633+jv40@users.noreply.github.com>
2024-01-17 23:19:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const songs = [anotherStar];
+
+//[0] = Artist Name, [1] = Song URL, [2] = Image URL
+const anotherStar = ["Stevie Wonder", "Bananas", "https://upload.wikimedia.org/wikipedia/en/e/e2/Songs_in_the_key_of_life.jpg"]
+
+for ()
+
+document.getElementById("#songs").appendChild(song).appendChild(anotherStar);
